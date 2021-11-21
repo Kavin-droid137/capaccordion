39bcb5257ddf4530170d16755dfc7761cee4c085
Kavin Ganesh R <77374022+Kavin-droid137@users.noreply.github.com>
2021-11-21 15:06:54 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,29 @@
+const questions = [
+    {
+        id: 1,
+        question: "What is the capital of India?",
+        answer: "Nulla dapibus vitae metus sit amet viverra. Sed condimentum leo ut velit gravida lacinia. Quisque pretium facilisis sem id mattis. Donec convallis est risus, ut volutpat magna laoreet vel. Nunc nec bibendum nibh. Nunc vestibulum elementum laoreet. Quisque tristique mi ac mauris fermentum condimentum.",
+    },
+    {
+        id: 2,
+        question: "What is the capital of USA?",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac augue vel nunc ornare volutpat. Mauris maximus commodo hendrerit. Sed vel efficitur quam, eget malesuada nibh. Nulla vitae enim vel sapien ultricies varius sed condimentum massa. Morbi pulvinar sed urna ac blandit. Suspendisse mollis egestas odio ut porta. "
+    },
+    {
+        id: 3,
+        question: "What is the capital of Canada?",
+        answer: "Vivamus pharetra ultrices dolor, ut lacinia quam interdum eu. Integer semper ornare orci ac hendrerit. Donec in nisi dolor. Quisque erat tellus, aliquet sit amet gravida vel, ullamcorper in diam. Praesent sed sem nulla. Aenean commodo vehicula sollicitudin. Nulla congue, nunc sit amet vestibulum venenatis, nisl risus pharetra nisl."
+    },
+    {
+        id: 4,
+        question: "What is the capital of Germany?",
+        answer: "Aenean gravida lobortis libero. Aenean cursus ipsum mauris. Nulla eu sapien massa. Donec nec justo sit amet est commodo ultrices ut in enim. Integer molestie, tortor et scelerisque convallis, nibh nisi commodo dolor, eget porttitor mauris dui ut augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+    },
+    {
+        id: 5,
+        question: "What is the capital of France?",
+        answer: "Cras aliquet tortor quis nulla fringilla varius. Sed neque diam, aliquet ac velit eu, finibus semper magna. Suspendisse potenti. Suspendisse suscipit sem non nibh porta interdum. Fusce consequat lectus augue, eget volutpat ipsum varius non.  Sed pulvinar gravida turpis, tempor rhoncus purus tempor ac. Nulla non felis nibh."
+    }
+]
+
+export default questions
